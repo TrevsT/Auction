a76aaee3052253fcafd7b446b0ca49cd351a3f44
Trevor Toy <trevorrist@gmail.com>
2018-08-02 23:40:32 +0200

    Moved back to ganache
    Removed 'gas' and fixed 'from' address becuase it takes to long to sync with Rinkeby each time so went back to ganache.

--- truffle.js
+++ truffle.js
@@ -6,9 +6,7 @@ module.exports = {
     development: {
       host: '127.0.0.1',
       port: 8545,
-      from: "0xeac3b27b03f5617148a138e6b986684a7527182f",
       network_id: '*', // Match any network id
-      gas: 5000000
     }
   }
 }
