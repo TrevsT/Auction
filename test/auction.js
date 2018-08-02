71b0ea51fa4a16fc67b1c765fa810ca953f0e954
Trevor Toy <trevorrist@gmail.com>
2018-08-02 23:51:01 +0200

    auctionEnd time test working
    problem was i was calling the auctionEnd function and not the public variable so was getting no return. Therefore i chnaged the names of the public variables.
    
    Also JS variables for time were set to string instead of int so used parseInt  to convert.

--- test/auction.js
+++ test/auction.js
@@ -4,22 +4,23 @@ var Auction = artifacts.require("./Auction.sol");
 
 contract('Auction', function(accounts) {
 
-    it("should assign the address of the contract deployer as the auction beneficiary", async function() {
+    it("should assign an address as the auction beneficiary when the contract is instantiated", async function() {
         var auction =  await Auction.deployed();
         var beneficiary = await auction.beneficiary.call();
         
-        assert.equal(beneficiary, accounts[2], "contract deployer not set as beneficiary");
+        assert.equal(beneficiary, accounts[0], "No beneficiary has been defined");
      });
 
      it("should set auction end time to current time + specified duration", async function() {
         var auction =  await Auction.deployed();
-        var auctionEndTime = await auction.auctionEnd.call();
-        var auctionStartTime = await auction.auctionStart.call();
+        //The contract output for uint numbers seems to output to a literal ('string') in javascript so parseInt is required to convert back to int
+        var auctionStart = parseInt(await auction.auctionStartTime.call());
+        var duration = parseInt(await auction.auctionDuration.call());
+        var auctionEnd = parseInt(await auction.auctionEndTime.call());
+      //AuctionStart/End names updated to AuctionStartTime/EndTime becuase JS was calling a function of the same name 'auctionEnd' 
+        console.log("Start Time: " + auctionStart + " EndTime: " + auctionEnd +  " Duration: " + duration);
 
-        console.log(auctionStartTime + " Start Time");
-        console.log(auctionEndTime + " EndTime");
-        
-        assert.equal(auctionEndTime, auctionStartTime,"Auction end time not set");
+        assert.equal(auctionEnd, auctionStart + duration,"Auction end time not set correctly");
 
       
      });
